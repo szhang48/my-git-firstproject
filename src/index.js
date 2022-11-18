e62b3c00199321219af572b98e53ab6dc0664d29
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-18 11:40:38 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,52 +1,82 @@
 
-
+function handleFetch() {
 fetch("http://localhost:3000/bobas")
 .then(res => res.json())
 .then(obj => renderbobas(obj))
-
+}
+handleFetch()
 
 const bobaMenu = document.querySelector("#boba-menu")
-const button = document.querySelector("#add-likes")
+const likeButton = document.querySelector("#add-likes")
 const like = document.querySelector('#likes-display')
 const detailImage = document.querySelector('.detail-image')
 const bobaName = document.querySelector('.name')
 const description = document.querySelector('.description')
 const dislike = document.querySelector('#dislikes-display')
-const button2 = document.querySelector("#add-dislikes")
+const dislikeButton = document.querySelector("#add-dislikes")
 
-
-button.addEventListener("click", () =>{
+let bobaId
+function buttonLikes(id){
+likeButton.addEventListener("click", () =>{
     const addLikes = parseInt(like.textContent) 
-    like.textContent = `${addLikes + 1 } Likes`
-    console.log(addLikes)
+    like.textContent = `${addLikes} Likes`
+    const currentLikes = parseInt(like.textContent.split(" ")[0])
+    console.log(currentLikes)
+    
 
-    fetch = (`http://localhost:3000/bobas/${id.likes}`, {
+    fetch(`http://localhost:3000/bobas/${likeButton.id}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(addLikes)
+        body: JSON.stringify({
+            likes: currentLikes +1
+        })
 
-        .then(res => res.json())
-        .then(addLikes => console.log(addLikes))
     })
-})
-button2.addEventListener('click', () => {
-    const addDislikes = parseInt(dislike.textContent) 
-    dislike.textContent = `${addDislikes + 1 } Dislikes`
-    console.log(addDislikes)
+    .then(res => res.json())
+    .then(obj => {
+        like.textContent = obj.likes
+    })
     
 })
+}
+buttonLikes()
+function buttonDislikes(id) {
+dislikeButton.addEventListener('click', () => {
+    const addDislikes = parseInt(dislike.textContent) 
+    dislike.textContent = `${addDislikes} Dislikes`
+    const currentDislikes = parseInt(dislike.textContent.split(" ")[0])
+
+    console.log(currentDislikes)
+    fetch(`http://localhost:3000/bobas/${dislikeButton.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            dislikes: currentDislikes +1
+        })
+
+    })
+    .then(res => res.json())
+    .then(obj => {
+       dislike.textContent = obj.dislikes
+    })
+    
+})
+}
+buttonDislikes()
 
 
-button.addEventListener('mouseover', (e) => {
+likeButton.addEventListener('mouseover', (e) => {
     e.target.style.color = "black";
     setTimeout(() => {
         e.target.style.color = '';
     }, 500)
 }, false);
 
-button2.addEventListener('mouseover', (e) => {
+dislikeButton.addEventListener('mouseover', (e) => {
     e.target.style.color = "black";
     setTimeout(() => {
         e.target.style.color = '';
@@ -63,8 +93,10 @@ function renderboba(boba){
     bobaMenu.append(bobaImg)
     
     bobaImg.addEventListener('click', (e) => { 
+        
         currentBoba = boba
-        console.log(boba.image)
+        likeButton.id = boba.id
+        dislikeButton.id = boba.id
         
         detailImage.src = boba.image
         bobaName.textContent = boba.name 
@@ -72,6 +104,8 @@ function renderboba(boba){
         like.textContent = boba.likes
         dislike.textContent = boba.dislikes
         
+        
+
         // bobaImg.querySelector('#likes-display').addEventListener("click",() => {
             //     boba.likes+=10
             //     bobaImg.querySelector('span').textContent = boba.likes
@@ -118,6 +152,28 @@ function handlePost(newBobaInput) {
     .then(res => res.json())
     .then(obj => console.log(obj))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function updateLikes(newBobaInput) {
 //     console.log(newBobaInput.id)
 //     fetch (`http://localhost:3000/bobas/${newBobaInput.id}`, {
